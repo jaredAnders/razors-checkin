4db2f98a1c1cae510f018e3ba5ad993ef0184a03
Jared Gilbert <jaredandersgilbert@gmail.com>
2017-05-10 22:17:59 -0400

    Add materialize, set colors, add nav and ul

--- client/app/bundles/App/components/App.jsx
+++ client/app/bundles/App/components/App.jsx
@@ -1,29 +1,13 @@
 import React from 'react'
-import PropTypes from 'prop-types'
 
 const App = ({ name, updateName }) => (
-  <div>
-    <form >
-      <label htmlFor="name">
-        Say hello to:
-      </label>
-      <input
-        id="name"
-        type="text"
-        value={name}
-        onChange={(e) => updateName(e.target.value)}
-      />
-    </form>
-    <hr />
-    <h3>
-      Hello, {name}!
-    </h3>
+  <div className='container'>
+    <ul className='collection'>
+      <a href='#' className='collection-item'>Test</a>
+      <a href='#' className='collection-item'>Test 2</a>
+      <a href='#' className='collection-item'>Test 3</a>
+    </ul>
   </div>
 )
-
-App.propTypes = {
-  name: PropTypes.string.isRequired,
-  updateName: PropTypes.func.isRequired,
-}
 
 export default App
