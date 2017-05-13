ef6d9ce9d14e3e33e6856667cc7b0d7793ac7eea
Jared Gilbert <jaredandersgilbert@gmail.com>
2017-05-13 13:25:58 -0400

    Add scrollable list and mock data, update colors

--- client/app/bundles/App/components/App.jsx
+++ client/app/bundles/App/components/App.jsx
@@ -1,11 +1,65 @@
 import React from 'react'
 
-const App = ({ name, updateName }) => (
+const testNames = [
+  {
+    firstName: 'Vito',
+    lastName: 'Corleone',
+    time: '3:00',
+  },
+  {
+    firstName: 'Sam',
+    lastName: 'Rothstein',
+    time: '3:45',
+  },
+  {
+    firstName: 'Henry',
+    lastName: 'Hill',
+    time: '3:15',
+  },
+  {
+    firstName: 'Tony',
+    lastName: 'Soprano',
+    time: '3:00',
+  },
+  {
+    firstName: 'Al',
+    lastName: 'Capone',
+    time: '4:00',
+  },
+  {
+    firstName: 'Lucky',
+    lastName: 'Luciano',
+    time: '3:30',
+  },
+  {
+    firstName: 'John',
+    lastName: 'Gotti',
+    time: '3:45',
+  },
+  {
+    firstName: 'Michael',
+    lastName: 'Corleone',
+    time: '3:00',
+  },
+]
+
+const listItems = testNames.map((name) => {
+  return (
+    <a className='collection-item'>
+      <span className='left'>
+        {name.firstName} {name.lastName}
+      </span>
+      <span className='right'>
+        {name.time}
+      </span>
+    </a>
+  )
+})
+
+const App = () => (
   <div className='container'>
-    <ul className='collection'>
-      <a href='#' className='collection-item'>Test</a>
-      <a href='#' className='collection-item'>Test 2</a>
-      <a href='#' className='collection-item'>Test 3</a>
+    <ul className='collection scrollable-list'>
+      {listItems}
     </ul>
   </div>
 )
