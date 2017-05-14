e66d16c4d3990d2201d06eb67c749937a849c1ba
Jared Gilbert <jaredandersgilbert@gmail.com>
2017-05-14 11:43:32 -0400

    Add checkedIn status to mock data and add button

--- client/app/bundles/App/components/App.jsx
+++ client/app/bundles/App/components/App.jsx
@@ -5,63 +5,101 @@ const testNames = [
   {
     firstName: 'Vito',
     lastName: 'Corleone',
-    time: '3:00',
+    time: '2:45',
+    checkedIn: true,
   },
   {
     firstName: 'Sam',
     lastName: 'Rothstein',
     time: '3:45',
+    checkedIn: false,
   },
   {
     firstName: 'Henry',
     lastName: 'Hill',
     time: '3:15',
+    checkedIn: true,
   },
   {
     firstName: 'Tony',
     lastName: 'Soprano',
     time: '3:00',
+    checkedIn: false,
   },
   {
     firstName: 'Al',
     lastName: 'Capone',
-    time: '4:00',
+    time: '4:30',
+    checkedIn: false,
   },
   {
     firstName: 'Lucky',
     lastName: 'Luciano',
     time: '3:30',
+    checkedIn: false,
   },
   {
     firstName: 'John',
     lastName: 'Gotti',
     time: '3:45',
+    checkedIn: false,
   },
   {
     firstName: 'Michael',
     lastName: 'Corleone',
     time: '3:00',
+    checkedIn: true,
   },
 ]
 
-const listItems = _.sortBy(testNames, ['time', 'firstName']).map((name) => {
+const listItems = _.sortBy(testNames, ['time', 'firstName']).map((name, i) => {
   return (
-    <a className='collection-item'>
+    <li className='collection-item'
+      key={i.toString() + name.lastName.slice(0,1) + name.firstName.slice(0,1)}>
       <span className='left'>
-        {name.firstName} {name.lastName}
+        {`${name.firstName} ${name.lastName} - ${name.time}`}
       </span>
       <span className='right'>
-        {name.time}
+        {name.checkedIn ?
+          <i className='grey-text checked-in material-icons'>
+            check
+          </i> :
+          <a className='btn waves-effect waves-light check-in-btn'>
+            Check In
+          </a>
+        }
       </span>
-    </a>
+    </li>
   )
 })
 
 const App = () => (
   <div className='container'>
+    {/* <div className='center'> */}
+      {/* <i className='material-icons grey-text'>
+        keyboard_arrow_up
+      </i> */}
+    {/* </div> */}
+    {/* <i className='material-icons scroll-arrow scroll-up'>
+      keyboard_arrow_up
+    </i>
+    <br/> */}
+    <div className='arrows right'>
+      <i className='material-icons grey-text'>
+        keyboard_arrow_up
+      </i>
+      {/* <br/> */}
+      <i className='material-icons grey-text'>
+        keyboard_arrow_down
+      </i>
+    </div>
+    <br/>
     <ul className='collection scrollable-list'>
       {listItems}
     </ul>
+    {/* <i className='material-icons scroll-arrow scroll-down'>
+      keyboard_arrow_down
+    </i> */}
   </div>
 )
 
